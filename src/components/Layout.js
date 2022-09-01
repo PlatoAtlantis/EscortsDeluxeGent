1129c53a95d7272c284311658f8085bb39775952
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2022-09-01 16:57:33 -0500

    .

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,39 +11,37 @@ const Layout = ({ children }) => {
       <Helmet>
         <html lang="en" />
         <title>Deluxe Escorts</title>
-        <meta name="description" content="deluxe" />
+        <meta name="description" content="Deluxe Escorts" />
 
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={`${withPrefix("/")}img/apple-touch-icon.png`}
+          href={`${withPrefix("/")}img/tiger.jpeg`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-32x32.png`}
-          sizes="32x32"
+          href={`${withPrefix("/")}img/tiger.jpeg`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-16x16.png`}
-          sizes="16x16"
+          href={`${withPrefix("/")}img/tiger.jpeg`}
         />
 
         <link
           rel="mask-icon"
-          href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
+          href={`${withPrefix("/")}img/tiger.jpeg`}
           color="#ff4400"
         />
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
-        <meta property="og:title" content="Deluxe" />
+        <meta property="og:title" content="Deluxe Escorts" />
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix("/")}img/og-image.jpg`}
+          content={`${withPrefix("/")}img/tiger.jpeg`}
         />
       </Helmet>
       <Navbar />
